1ad660f89a66ebcaceae60f3385242424d1af520
ersin <ersinblt61@gmail.com>
2022-01-31 13:14:59 +0300

    #16 Asp.Net Core 5.0 Proje Kampı Generic Repository

---
+++ DataAccessLayer/Repositories/CommentRepository.cs
@@ -0,0 +1,38 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class CommentRepository : IGenericDal<Comments>
+    {
+        public void Delete(Comments t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Comments GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Comments> GetListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Comments t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Comments t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
