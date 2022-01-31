5e22848eeb597d25f9370bd327d5182668ddc7dd
ersin <ersinblt61@gmail.com>
2022-01-31 11:45:04 +0300

    #14 Asp.Net Core 5.0 Proje Kampı Interfacelerin Tanımlanması

---
+++ DataAccessLayer/Abstract/IProductDal.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IProductDal
+    {
+        List<Product> ListAllCategory();
+        void AddProduct(Product product);
+        void DeleteProduct(Product product);
+        void UpdateProduct(Product product);
+        Product GetById(int id);
+    }
+}
