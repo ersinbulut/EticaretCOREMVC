a4ec9d42cf5f7cac77528def89985e76f48bb81f
Batuhan Sarıkaya <batuhanadaca@gmail.com>
2022-02-07 15:30:47 +0300

    (batuhan) refactoring

---
+++ BusinessLayer/Abstract/ICommentService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+	public interface ICommentService:IGenericService<Comments>
+	{
+		//void CommentAdd(Comments comments);
+		//void CommentDelete(Comments comments);
+		//void CommentUpdate(Comments comments);
+		//List<Comments> GetList();
+		//Comments GetById(int id);
+	}
+}
---
+++ CoreEticaret/Controllers/CommentController.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CoreEticaret.Controllers
+{
+	public class CommentController : Controller
+	{
+		CommentManager commentManager = new CommentManager(new EfCommentRepository());
+		[HttpGet]
+		public PartialViewResult CommentAddPartial(int id)
+		{
+			var product = commentManager.GetById(id);
+			return PartialView(product);
+		}
+		[HttpPost]
+		public PartialViewResult CommentAddPartial(Comments comment)
+		{
+			comment.Name = "Batuhan";
+			comment.AddedDate =DateTime.Parse(DateTime.Now.ToShortDateString());
+			comment.IsApproved = true;
+			comment.Status = true;
+			comment.ProductId = 3;
+			commentManager.TAdd(comment);
+			return PartialView();
+		}
+	}
+}
