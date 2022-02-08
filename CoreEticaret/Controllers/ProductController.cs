a9d22e8704d1727c93b4047a3c0c0658d436064e
ersin bulut <ersinblt61@gmail.com>
2022-02-08 10:16:53 +0300

    ersin sınırsız kategori islemleri listeleme calismiyor...

--- CoreEticaret/Controllers/ProductController.cs
+++ CoreEticaret/Controllers/ProductController.cs
@@ -30,6 +30,24 @@ namespace CoreEticaret.Controllers
             var values = pm.GetProductListWithCategory();
             return View(values);
         }
+        public PartialViewResult _CategoryList()
+        {
+            var kategoriler = db.Categories.Include(i => i.Products).Select(x => new CategoryModel()
+            //var kategoriler = db.Categories.Select(x => new Category()
+            {
+                Id = x.Id,
+                ParentId = x.ParentId,
+                Name = x.Name,
+                Count = x.Products.Count()
+            }
+            ).ToList();
+            return PartialView(kategoriler);
+
+            //List<Category> all = new List<Category>();
+            //all = db.Categories.OrderBy(a => a.ParentId).ToList();
+            //return PartialView(all);
+        }
+
 
     }
 }
