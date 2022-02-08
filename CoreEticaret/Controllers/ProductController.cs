bbd88ebf4bfc7477b1eff9b9c4757043d2610f90
ersin bulut <ersinblt61@gmail.com>
2022-02-08 10:13:57 +0300

    ersin sınırsız kategori işlemleri listeleme çalışmıyor..

--- CoreEticaret/Controllers/ProductController.cs
+++ CoreEticaret/Controllers/ProductController.cs
@@ -1,12 +1,17 @@
 ﻿using BusinessLayer.Concrete;
+using CoreEticaret.Models;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace CoreEticaret.Controllers
 {
     public class ProductController : Controller
     {
         ProductManager pm = new ProductManager(new EfProductRepository());
+        Context db = new Context();
         public IActionResult Index()
         {
             var values = pm.GetProductListWithCategory();
@@ -22,6 +27,24 @@ namespace CoreEticaret.Controllers
         {
             var values = pm.GetProductListWithCategory();
             return View(values);
+        }
+
+        public PartialViewResult _CategoryList()
+        {
+            var kategoriler = db.Categories.Include(i => i.Products).Select(x => new CategoryModel()
+            //var kategoriler = db.Categories.Select(x => new Category()
+            {
+                Id = x.Id,
+                ParentId = x.ParentId,
+                Name = x.Name,
+                Count = x.Products.Count()
+            }
+            ).ToList();
+            return PartialView(kategoriler);
+
+            //List<Category> all = new List<Category>();
+            //all = db.Categories.OrderBy(a => a.ParentId).ToList();
+            //return PartialView(all);
         }
 
     }
